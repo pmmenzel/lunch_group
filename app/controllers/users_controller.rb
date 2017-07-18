9a68d5d0babdc72b4ecddd6bb189b02415af7be1
Patrick M. Menzel <pmmenzel@gmail.com>
2017-07-17 19:17:41 -0700

    Add basic user controller and views as well as seed file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = 'New User has been created'
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = Model.find(params[:id])
+  end
+
+  def destroy
+    @user = Model.find(params[:id]).destroy
+    flash[:notice] = 'User has been Deleted!'
+
+    redirect_to '/'
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+50.times do
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.first_name
+    )
+end
+
+p "#{User.count} New users have been created!"
