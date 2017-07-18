138bd66f039bcbae061766ee28ff4bc35ae25d2d
Patrick M. Menzel <pmmenzel@gmail.com>
2017-07-17 19:11:08 -0700

    add user model and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  attr_reader :name
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  def name
+    [first_name, last_name].join(' ')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # ==== Removing/Adding back (do some ordering)
+  root "users#index"
 
+  resources :users
 end
